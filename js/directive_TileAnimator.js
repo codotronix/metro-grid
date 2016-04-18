b520ea3603f6bdd10053a841362ebceaf86e1b43
Suman Barick <codotronix@gmail.com>
2016-04-18 14:45:13 +0530

    Tile Updates

--- js/directive_TileAnimator.js
+++ js/directive_TileAnimator.js
@@ -9,15 +9,20 @@
                 $scope.AnimateClass = 'bounce';
                 
                 var animationClasses = ["slideInDown", "slideInLeft", "slideInRight", "slideInUp", "rotateIn", "rollIn", "zoomIn"];
-                var count = 0;
+                //var count = 0;
+                var waitTime = 2500 + Math.round(Math.random() * (5000 / $scope.tile.liveImgUrls.length));
                 function nxtImg() {
-                    if(!$rootScope.flags.tileMovementAllowed && !$rootScope.flags.tileResizingAllowed) {
+                    if (!$rootScope.flags.tileMovementAllowed && !$rootScope.flags.tileResizingAllowed) {
                         $scope.currImgIndex = ($scope.currImgIndex + 1) % $scope.tile.liveImgUrls.length;
-                        
-                        count = (count + 1) % animationClasses.length;
-                        $scope.AnimateClass = animationClasses[count];
+                        //console.log($scope.tile.id + " has waitTime = " + waitTime);
+                        //count = (count + 1) % animationClasses.length;
+                        $scope.AnimateClass = animationClasses[Math.floor(Math.random() * animationClasses.length)];
+                    }
+                    
+                    //if there are more than 1 image, call next image, otherwise stop here
+                    if ($scope.tile.liveImgUrls.length > 1) {
+                        $timeout(nxtImg, waitTime);
                     }                    
-                    $timeout(nxtImg, 2500);
                 }
                 
                 nxtImg();
